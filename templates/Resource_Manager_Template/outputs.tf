027c7c5634a0ad8441b70aebe9e25cc8c332edaf
Shahana Baliyambra <shahana.baliyambra@crowdstrike.com>
2025-01-28 21:52:18 -0800

    updated version

--- templates/Resource_Manager_Template/outputs.tf
+++ templates/Resource_Manager_Template/outputs.tf
@@ -4,7 +4,7 @@ output "user_ocid" {
 }
 
 output "template_version" {
-  value = "v0.3.1"
+  value = "v0.3.2"
   description = "The version of CrowdStrike's OCI integration supported by this template."
 }
 
