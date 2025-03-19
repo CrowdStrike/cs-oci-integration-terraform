e0c82d32ce462f49089eeb9e1362fa598e6c15ec
Shahana Baliyambra <shahana.baliyambra@crowdstrike.com>
2025-03-18 21:56:56 -0700

    test another module

---
+++ templates/Resource_Manager_Template/modules/test-ioa/outputs.tf
@@ -0,0 +1,9 @@
+output "user_ocid" {
+  value       = oci_identity_user.fcs_inventory_user.id
+  description = "OCID of user created for use by CrowdStrike"
+}
+
+output "template_version" {
+  value       = "v0.3.5"
+  description = "The version of CrowdStrike's OCI integration supported by this template"
+}
