453c78bd47759efbd57df5c71dd03718f5f9b5cc
hraveendran-cs <harigovind.raveendran@crowdstrike.com>
2025-05-16 09:23:27 +0100

    Update filestorage, vault and key permissions (#20)
    * Update permissions for filestorage, key and vault

--- templates/Resource_Manager_Template/modules/iom/main.tf
+++ templates/Resource_Manager_Template/modules/iom/main.tf
@@ -124,7 +124,10 @@ resource "oci_identity_policy" "fcs_inventory_policy_without_domains" {
     "Allow group ${var.group_name} to read instances in tenancy",
     "Allow group ${var.group_name} to read buckets in tenancy",
     "Allow group ${var.group_name} to read virtual-network-family in tenancy",
-    "Allow group ${var.group_name} to inspect autonomous-database-family in tenancy"
+    "Allow group ${var.group_name} to inspect autonomous-database-family in tenancy",
+    "Allow group ${var.group_name} to read vaults in tenancy",
+    "Allow group ${var.group_name} to read keys in tenancy",
+    "Allow group ${var.group_name} to read file-family in tenancy"
   ]
 }
 
@@ -146,7 +149,10 @@ resource "oci_identity_policy" "fcs_inventory_policy_with_domains" {
     "Allow group 'Default'/'${var.group_name}' to read instances in tenancy",
     "Allow group 'Default'/'${var.group_name}' to read buckets in tenancy",
     "Allow group 'Default'/'${var.group_name}' to read virtual-network-family in tenancy",
-    "Allow group 'Default'/'${var.group_name}' to inspect autonomous-database-family in tenancy"
+    "Allow group 'Default'/'${var.group_name}' to inspect autonomous-database-family in tenancy",
+    "Allow group 'Default'/'${var.group_name}' to read vaults in tenancy",
+    "Allow group 'Default'/'${var.group_name}' to read keys in tenancy",
+    "Allow group 'Default'/'${var.group_name}' to read file-family in tenancy"
   ]
 }
 
