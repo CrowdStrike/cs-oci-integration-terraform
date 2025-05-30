3869ead24d84aa6758d2a67951e32d8c245dd59a
Daniel Mapleson <daniel.mapleson@crowdstrike.com>
2025-05-30 15:58:52 +0100

    remove inspect permission as its redundant

--- templates/Resource_Manager_Template/modules/iom/main.tf
+++ templates/Resource_Manager_Template/modules/iom/main.tf
@@ -128,7 +128,6 @@ resource "oci_identity_policy" "fcs_inventory_policy_without_domains" {
     "Allow group ${var.group_name} to read vaults in tenancy",
     "Allow group ${var.group_name} to read keys in tenancy",
     "Allow group ${var.group_name} to read file-family in tenancy",
-    "Allow group ${var.group_name} to inspect cluster-family in tenancy",
     "Allow group ${var.group_name} to read cluster-family in tenancy"
   ]
 }
@@ -155,7 +154,6 @@ resource "oci_identity_policy" "fcs_inventory_policy_with_domains" {
     "Allow group 'Default'/'${var.group_name}' to read vaults in tenancy",
     "Allow group 'Default'/'${var.group_name}' to read keys in tenancy",
     "Allow group 'Default'/'${var.group_name}' to read file-family in tenancy",
-    "Allow group 'Default'/'${var.group_name}' to inspect cluster-family in tenancy",
     "Allow group 'Default'/'${var.group_name}' to read cluster-family in tenancy"
   ]
 }
