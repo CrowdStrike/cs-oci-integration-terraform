82c3b6d8c4e5558032bdb8b9ab6f429c6725b04f
Daniel Mapleson <daniel.mapleson@crowdstrike.com>
2025-06-06 16:40:34 +0100

    feat(permissions): adding permissions for cloud events rules
    CSPG-61826

--- templates/Resource_Manager_Template/modules/iom/outputs.tf
+++ templates/Resource_Manager_Template/modules/iom/outputs.tf
@@ -4,6 +4,6 @@ output "user_ocid" {
 }
 
 output "template_version" {
-  value       = "v0.3.13"
+  value       = "v0.3.14"
   description = "The version of CrowdStrike's OCI integration supported by this template"
 }
