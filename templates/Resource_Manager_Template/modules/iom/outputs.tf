9f6290513bc925215bb7a104c8d3bd554a81b4b9
Doron Moshel <doron.moshel@crowdstrike.com>
2025-07-06 19:30:33 +0300

    update oci template_version to v0.3.16

--- templates/Resource_Manager_Template/modules/iom/outputs.tf
+++ templates/Resource_Manager_Template/modules/iom/outputs.tf
@@ -4,6 +4,6 @@ output "user_ocid" {
 }
 
 output "template_version" {
-  value       = "v0.3.15"
+  value       = "v0.3.16"
   description = "The version of CrowdStrike's OCI integration supported by this template"
 }
