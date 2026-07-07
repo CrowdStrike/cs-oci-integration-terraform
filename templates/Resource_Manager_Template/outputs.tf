dcd0ca8ecee74fa44190dca140b2ffd929a68b40
Alex Grant <alex.grant@crowdstrike.com>
2026-07-07 11:09:09 -0400

    fix: format Terraform files to pass CI checks

--- templates/Resource_Manager_Template/outputs.tf
+++ templates/Resource_Manager_Template/outputs.tf
@@ -1,5 +1,5 @@
 output "user_ocid" {
-  value = module.iom.user_ocid
+  value       = module.iom.user_ocid
   description = "OCID of user created for use by CrowdStrike. Copy this OCID and paste into last step of OCI tenancy registration wizard in CrowdStrike to finish tenancy registration process."
 }
 
